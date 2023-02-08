fd093e1e3249389b26f4b408aa08357144f39fe0
mickey <shoey25ms@gmail.com>
2023-02-08 10:03:52 -0500

    Update

--- js/palindrome.js
+++ js/palindrome.js
@@ -1,3 +1,20 @@
-exports.palindrome = function(word) {
+/*
+1. Input cleaning - change to string, remove whitespace or punctuation, and change to lowercase
+2. Loop through half the indexes
+3. Make counter - point to last index, decrement each loop
+4. If at anytime the char in the loop and the counter do not match , return false
+5. If it runs through return true
 
+*/
+
+exports.palindrome = function(word) {
+  new_word = word.toString().replace(/[^\w]/g, "").toLowerCase();
+  let counter = new_word.length - 1;
+  for (let i=0; i<new_word.length; i++) {
+    if (new_word[i] !== new_word[counter]) {
+      return false
+    }
+    counter --
+  }
+  return true
 };
--- python/palindrome.py
+++ python/palindrome.py
@@ -1,2 +1,10 @@
+# O(n) - but only half n, as it does constant time access to opposite index
+import re
+
 def palindrome(word):
-    # Write code here
+    new_word = re.sub(r'[^\w]','', str(word)).lower()
+    for i in range(len(new_word)//2):
+        if new_word[i] != new_word[-(i+1)]:
+            return False
+    return True
+  
